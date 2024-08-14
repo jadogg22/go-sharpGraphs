53cf4113ea91af3c1a4c7a56e3d568f09281efa9
Jaden @ work <jaden@sharptrucking.com>
2024-08-14 15:23:30 -0600

    Starting to use nginx

--- Deploy.sh
+++ Deploy.sh
@@ -1,15 +1,12 @@
+
 #!/bin/bash
 
 # Define variables
-CONTAINER_NAME="my-go-server"
-IMAGE_NAME="my-go-server"
-HOST_DATA_DIR="/home/jaden/goServe/Data"
-CONTAINER_DATA_DIR="/app/Data"
+COMPOSE_FILE="docker-compose.yml"
 
-# Stop and remove the existing container if it's running
-echo "Stopping and removing existing container..."
-docker stop $CONTAINER_NAME
-docker rm $CONTAINER_NAME
+# Stop and remove existing containers and networks
+echo "Stopping and removing existing containers and networks..."
+docker-compose down
 
 # Navigate to the project directory
 echo "Navigating to project directory..."
@@ -21,27 +18,18 @@ git pull
 
 # Check if the pull was successful
 if [ $? -ne 0 ]; then
-        echo "Git pull failed"
-        exit 1
+    echo "Git pull failed"
+    exit 1
 fi
 
-# Build the Docker image
-echo "Building Docker image..."
-docker build -t $IMAGE_NAME .
+# Build and start the Docker containers
+echo "Building and starting Docker containers..."
+docker-compose up --build -d
 
-if [ $? -ne 0 ]; then
-        echo "Image build failed"
-        exit 1
-fi
-
-# Run the Docker container with the specified volume mount
-echo "Running Docker container..."
-docker run -d -p 5000:5000 --name $CONTAINER_NAME -v $HOST_DATA_DIR:$CONTAINER_DATA_DIR $IMAGE_NAME
-
-
-#check the status of last command
+# Check the status of the last command
 if [ $? -eq 0 ]; then
-        echo "Deployment completed successfully"
+    echo "Deployment completed successfully"
 else
-        echo " Command failed with exit code $?"
+    echo "Command failed with exit code $?"
 fi
+
